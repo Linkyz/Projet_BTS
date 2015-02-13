5e95032e95839c6dbe228350433f778f9ff82fa3
Linkyz <v3_pascal@hotmail.com>
2015-02-13 19:49:24 +0100

    Index_css
    Début index et  css pour le site

---
+++ Projet_Site/index.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<link rel="stylesheet" type="text/css" href="main.css">
+		<meta charset="UTF-8"/>
+	</head>
+	<body>
+		<header>
+			<nav id="menu_top">
+				<ul class="niveau1">
+					<li>Menu1</li>
+					<li>Menu1</li>
+					<li><img src=""/></li>
+					<li>Menu1</li>
+					<li>Menu1
+						<ul class="niveau2">
+							<form id="login"/>
+								<li>Login: <input type="text" value="identifiant" id="login_id"/></li>
+								<li>Mot de passe: <input type="password" id="login_pass"/></li>
+								<li><input type="submit" value="Valider"/></li>
+							</form>
+						</ul>
+					</li>
+				</ul>
+			</nav>
+		</header>
+		<aside id="menu_left" >
+			<ul class="niveau1">
+				<li>
+					L<br/>
+					I<br/>
+					G<br/>
+					U<br/>
+					E<br/>
+					S
+					<ul class="niveau2">
+						<li><img src="boule_test.png"/></li>
+						<li><img src="boule_test.png"/></li>
+						<li><img src="boule_test.png"/></li>
+						<li><img src="boule_test.png"/></li>
+						<li><img src="boule_test.png"/></li>
+						<li><img src="boule_test.png"/></li>
+					</ul>
+				<li>
+			</ul>
+		</aside>
+	</body>
+</html>
